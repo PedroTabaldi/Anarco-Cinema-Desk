38cbbc83a2b275505862488a3330194e031178e1
PEDRO AUGUSTO PEDROSO TABALDI <05796912097@senacrs.edu.br>
2025-09-17 21:14:18 -0300

    first commit

---
+++ Anarco-Cinema-Desk/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Anarco_Cinema_Desk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Anarco-Cinema-Desk/Logar.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Anarco_Cinema_Desk
+{
+    /// <summary>
+    /// Interação lógica para Logar.xam
+    /// </summary>
+    public partial class Logar : Page
+    {
+        public Logar()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Inserir_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void bt_logar_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Catalogo());
+            
+        }
+
+        private void bt_filme_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Catalogo());
+        }
+    }
+}
