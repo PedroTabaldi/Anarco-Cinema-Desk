b06f12f12b01037469545c152dbf7f8ae85df6ef
PedroTabaldi <pedroaotabaldi@gmail.com>
2025-12-12 20:59:04 -0300

    a

--- Anarco-Cinema-Desk/obj/Release/net8.0-windows/Anarco-Cinema-Desk.AssemblyInfo.cs
+++ Anarco-Cinema-Desk/obj/Release/net8.0-windows/Anarco-Cinema-Desk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anarco-Cinema-Desk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e99987fb68a5bdf1dd1d4e2a1ad668d5becc1e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9d7f83d306975450f1d247c944bc1cf3b48ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anarco-Cinema-Desk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anarco-Cinema-Desk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
